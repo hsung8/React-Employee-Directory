572f4cf5cc5342b48aacedfeb36981823daa443a
hsung8 <hsung@atlantaregional.org>
2020-10-19 21:45:37 -0400

    init

---
+++ employeedirectory/src/components/Wrapper/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "../Main/index"
+import Header from "../Header/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
+}
+export default App;
